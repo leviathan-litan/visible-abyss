8676bd736a7fbafeb103489b708fa9721ad8bf4d
Leviathan <leviathan_litan@163.com>
2024-06-19 20:15:21 +0800

    修改「前端框架」的结构

--- visible_abyss_frontend_vuejs/src/main.ts
+++ visible_abyss_frontend_vuejs/src/main.ts
@@ -5,7 +5,7 @@ import '@arco-design/web-vue/dist/arco.css'
 
 // =========================== 导入包
 import { createApp} from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 // =========================== Arco.design
 import ArcoVue from "@arco-design/web-vue";
@@ -15,10 +15,18 @@ import ArcoVueIcon from "@arco-design/web-vue/es/icon"
 import "virtual:svg-icons-register"
 
 // =========================== 导入文件
+
+// --- 实例化对象
 import App from './App.vue'
+
+// --- 路由
 import router from './router'
 
+// --- 国际化支持
 import i18n from '@/i18n/index'
+
+// --- 持久化「Pinia」
+import store from './stores'
 
 // =========================== 测试
 // 显示「环境变量」
@@ -32,8 +40,10 @@ app.use(ArcoVue, {
     componentPrefix: 'arco'
 })
 app.use(ArcoVueIcon)
-app.use(createPinia())
+// app.use(createPinia())
 app.use(router)
+app.use(i18n)
+app.use(store)
 
 // =========================== 挂载「Vue.JS」实例到「DOM容器」
 app.mount('#app')
